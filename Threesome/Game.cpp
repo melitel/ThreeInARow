6b44db7c2f6f75461dda28bac3c6a61484a81dbe
melitel <melitel.avatar@gmail.com>
2022-11-26 19:28:44 +0200

    v 2.0

--- Threesome/Game.cpp
+++ Threesome/Game.cpp
@@ -1,9 +1,6 @@
 #include "Game.h"
 #include<iostream>
-
-sf::Color g_colors[] = {
-	sf::Color::Yellow, sf::Color::Green, sf::Color::Red, sf::Color::Blue, sf::Color::Magenta
-};
+#include <cmath>
 
 const uint32_t INVALID_INDEX = uint32_t(-1);
 
@@ -45,30 +42,46 @@ void Game::run()
 
 }
 
+void Game::load_textures(std::string file_path, Game::gem_color index) {
+
+	if (!m_colors[index].loadFromFile(file_path))
+	{
+		//error::
+	}
+
+}
+
 void Game::initialize()
 {
-	uint32_t n = 8;
-	float board_size = (n * (m_gem_size * 2)) + ((n + 1) * gem_offset);
-	m_board.setSize(sf::Vector2f(board_size, board_size));
+	float board_width = ((m_rows * m_gem_side) + ((m_rows + 1) * gem_offset));
+	float board_heights = ((m_columns * m_gem_side) + ((m_columns + 1) * gem_offset));
+	m_board.setSize(sf::Vector2f(board_width, board_heights));
 	m_board.setFillColor(sf::Color::Black);
 	m_board.setPosition(sf::Vector2f(0.f, 0.f));
 	m_window.draw(m_board);
 
-	for (uint32_t i = 0; i < n; i++) {
+	load_textures("gem1.png", gc_orange);
+	load_textures("gem6.png", gc_green);
+	load_textures("gem3.png", gc_red);
+	load_textures("gem4.png", gc_blue);
+	load_textures("gem5.png", gc_violet);
+	load_textures("gem2.png", gc_black);
 
-		const float offset_gem_i = gem_offset + (m_gem_size*2 + gem_offset) * i;
-		
-		for (uint32_t j = 0; j < n; j++)
+	for (uint32_t i = 0; i < m_rows; i++) {
+
+		const float offset_gem_i = gem_offset + (m_gem_side + gem_offset) * i;
+
+		for (uint32_t j = 0; j < m_columns; j++)
 		{
-			const int gem_index = i + j * n;
-			const float offset_gem_j = gem_offset + (m_gem_size*2 + gem_offset) * j;			
+			const int gem_index = i + j * m_columns;
+			const float offset_gem_j = gem_offset + (m_gem_side + gem_offset) * j;
 
 			Gem& gem = m_gems_array[gem_index];
 			gem.color = (gem_color)distr(gen);
-			gem.circle.setRadius(m_gem_size);
-			gem.circle.setFillColor(g_colors[gem.color]);
-			gem.circle.setPosition(m_board.getPosition()+sf::Vector2f(offset_gem_i, offset_gem_j));
-			
+			gem.rect.setSize({ m_gem_side , m_gem_side });
+			gem.rect.setTexture(&m_colors[gem.color]);
+			gem.rect.setPosition(m_board.getPosition() + sf::Vector2f(offset_gem_i, offset_gem_j));
+
 		}
 	}
 
@@ -83,8 +96,8 @@ void Game::draw()
 	m_window.draw(m_board);
 
 	for (int i = 0; i < m_gems_array.size(); i++)
-	{		
-		m_window.draw(m_gems_array[i].circle);
+	{
+		m_window.draw(m_gems_array[i].rect);
 	}
 
 	m_window.display();
@@ -97,20 +110,94 @@ void Game::update()
 		gem_color color = m_gems_array[m_selected_index_1].color;
 		m_gems_array[m_selected_index_1].color = m_gems_array[m_selected_index_2].color;
 		m_gems_array[m_selected_index_2].color = color;
+		m_gems_array[m_selected_index_1].rect.setOutlineThickness(0);
 
-		m_selected_index_1 = INVALID_INDEX;
-		m_selected_index_2 = INVALID_INDEX;
+		uint32_t move_valid = 0;
+
+		searchCol();
+
+		for (int i = 0; i < m_gems_to_destroy.size(); ++i)
+		{
+			if (m_gems_to_destroy[i] == m_selected_index_1 || m_gems_to_destroy[i] == m_selected_index_2)
+			{
+				move_valid = 1;
+			}			
+		}
+
+		if (move_valid == 1)
+		{
+			m_selected_index_1 = INVALID_INDEX;
+			m_selected_index_2 = INVALID_INDEX;
+			move_valid = 0;
+		}
+		else {
+			m_gems_to_destroy.clear();
+			m_gems_array[m_selected_index_2].color = m_gems_array[m_selected_index_1].color;
+			m_gems_array[m_selected_index_1].color = color;
+			m_selected_index_1 = INVALID_INDEX;
+			m_selected_index_2 = INVALID_INDEX;
+		}
+
+
+
+// 		m_gems_array[m_selected_index_1].rect.setOutlineThickness(0);
+// 		m_selected_index_1 = INVALID_INDEX;
+// 		m_selected_index_2 = INVALID_INDEX;
+
 	}
+
+	if (!m_gems_to_destroy.empty()) {
+		for (auto it = m_gems_to_destroy.cbegin(); it != m_gems_to_destroy.cend(); ++it)
+		{
+			uint32_t index = *it;
+
+			m_gems_array[index].color = gem_color::gc_black;
+
+		}
+	}
+
+	if (!m_gems_to_destroy.empty()) {
+
+		for (int i = 0; i < m_rows; ++i)
+		{
+			int index = m_columns * (m_rows - 1) + i;
+
+			for (int j = index; j >= 0; j -= 8)
+			{
+				if (m_gems_array[j].color == gem_color::gc_black)
+				{					
+					int element_to_swap_with = j;
+
+					while (element_to_swap_with > 0 && m_gems_array[element_to_swap_with].color == gem_color::gc_black)
+					{
+						element_to_swap_with -= 8;
+					}
+
+					if (element_to_swap_with < 0 || element_to_swap_with == 0)
+					{
+						m_gems_array[j].color = (gem_color)distr(gen);
+					}
+					else {
+						m_gems_array[j].color = m_gems_array[element_to_swap_with].color;
+						m_gems_array[element_to_swap_with].color = gem_color::gc_black;
+					}
+
+				}
+			}
+		}
+	}
+
+	m_gems_to_destroy.clear();
 
 	for (int i = 0; i < m_gems_array.size(); i++)
 	{
 		Gem& gem = m_gems_array[i];
-		gem.circle.setFillColor(g_colors[gem.color]);
+		gem.rect.setTexture(&m_colors[gem.color]);
 	}
-	
+
 }
 
-void Game::gather_input(const sf::Vector2i &mouse_position) {
+void Game::gather_input(const sf::Vector2i& mouse_position) {
 
 	uint32_t index = get_index_by_mouse_position(mouse_position);
 
@@ -118,11 +205,20 @@ void Game::gather_input(const sf::Vector2i &mouse_position) {
 	if (index != INVALID_INDEX && m_selected_index_1 == INVALID_INDEX)
 	{
 		m_selected_index_1 = index;
+		m_gems_array[m_selected_index_1].rect.setOutlineThickness(2);
+		//m_gems_array[m_selected_index_1].circle.setOutlineColor(sf::Color(255, 255, 255, 255));
 
 	}
 	else if (index != INVALID_INDEX && m_selected_index_2 == INVALID_INDEX && m_selected_index_1 != INVALID_INDEX) {
 
-		m_selected_index_2 = index;
+		if (std::abs((int)m_selected_index_1 - (int)index) == 1 || std::abs((int)m_selected_index_1 - (int)index) == 8)
+		{
+			m_selected_index_2 = index;
+		}
+		else if (m_selected_index_1 == index) {
+			m_gems_array[m_selected_index_1].rect.setOutlineThickness(0);
+			m_selected_index_1 = INVALID_INDEX;
+		}
 	}
 }
 
@@ -131,7 +227,7 @@ uint32_t Game::get_index_by_mouse_position(const sf::Vector2i& mouse_position)
 	for (int i = 0; i < m_gems_array.size(); i++)
 	{
 		const Gem& gem = m_gems_array[i];
-		sf::FloatRect gem_bounds = gem.circle.getGlobalBounds();
+		sf::FloatRect gem_bounds = gem.rect.getGlobalBounds();
 
 		if (gem_bounds.contains(sf::Vector2f((float)mouse_position.x, (float)mouse_position.y))) {
 			return i;
@@ -139,4 +235,79 @@ uint32_t Game::get_index_by_mouse_position(const sf::Vector2i& mouse_position)
 
 	}
 	return INVALID_INDEX;
+}
+
+void Game::searchCol()
+{
+	for (uint32_t i = 0; i < m_rows * m_columns; i += 8)
+	{
+		uint32_t consegativeRepeat = 0;
+		gem_color color = m_gems_array[i].color;
+
+		for (uint32_t j = 0; j < m_rows; ++j)
+		{
+			uint32_t index = i + j;
+
+			if (m_gems_array[index].color == color) {
+				++consegativeRepeat;
+
+				if ((j+1)%8 == 0 && consegativeRepeat >= 3)
+				{
+					for (uint32_t k = 0; k < consegativeRepeat; ++k)
+					{
+						m_gems_to_destroy.push_back(index - k);
+					}
+				}
+			}
+			else {
+
+				if (consegativeRepeat >= 3)
+				{
+					for (uint32_t k = 0; k < consegativeRepeat; ++k)
+					{						
+						m_gems_to_destroy.push_back(index - (1 + k));
+					}
+				}
+
+				consegativeRepeat = 1; //1 BECAUSE WE FOUND ONE COLOR
+				color = m_gems_array[index].color;
+			}
+		}
+	}
+
+	for (uint32_t i = 0; i < m_columns; ++i)
+	{
+		uint32_t consegativeRepeat = 0;
+		gem_color color = m_gems_array[i].color;
+
+		for (uint32_t j = 0; j < m_rows * m_columns; j += 8)
+		{
+			uint32_t index = i + j;
+
+			if (m_gems_array[index].color == color) {
+				++consegativeRepeat;
+
+				if (j == (m_rows*(m_columns - 1)) && consegativeRepeat >= 3)
+				{
+					for (uint32_t k = 0; k < consegativeRepeat; ++k)
+					{
+						m_gems_to_destroy.push_back(index - (8 * k));
+					}
+				}
+			}
+			else {
+
+				if (consegativeRepeat >= 3)
+				{
+					for (uint32_t k = 0; k < consegativeRepeat; ++k)
+					{
+						m_gems_to_destroy.push_back(index - (8 * (k + 1)));
+					}
+				}
+
+				consegativeRepeat = 1; //1 BECAUSE WE FOUND ONE COLOR
+				color = m_gems_array[index].color;
+			}
+		}
+	}
 }
--- Threesome/Game.h
+++ Threesome/Game.h
@@ -18,29 +18,36 @@ private:
 
 	void gather_input(const sf::Vector2i& mouse_position);
 	uint32_t get_index_by_mouse_position(const sf::Vector2i& mouse_position);
-	enum gem_color { gc_yellow, gc_green, gc_red, gc_blue, gc_violet };
+	enum gem_color { gc_orange, gc_green, gc_red, gc_blue, gc_violet, gc_black, gc_count };
+	void load_textures(std::string file_path, gem_color index);
+	void searchCol();
 
 	struct Gem {
-		sf::CircleShape circle;
+		sf::RectangleShape rect;
 		gem_color color;
 	};
 
 	//SFML graphics objects
 	const uint32_t m_window_width = 500;
 	const uint32_t m_window_height = 500;
-	const float m_gem_size = 24.f;
+	const float m_gem_side = 48.f;
 	const float gem_offset{ 5 };
 	bool m_success_move{ false };
 	uint32_t m_selected_index_1;
 	uint32_t m_selected_index_2;
+	uint32_t m_rows{ 8 };
+	uint32_t m_columns{ 8 };
 
 	sf::RenderWindow m_window{ sf::VideoMode(sf::Vector2u(m_window_width, m_window_height)), "My window" };
 	sf::RectangleShape m_upper_border;
 	sf::RectangleShape m_lower_border;
 	sf::RectangleShape m_board;
-	sf::CircleShape m_gem{ m_gem_size };
+	sf::RectangleShape m_gem;
+	sf::Texture m_gems_texture;	
 
 	std::array<Game::Gem, 64> m_gems_array;
+	std::array<sf::Texture, gc_count> m_colors;
+	std::vector<uint32_t> m_gems_to_destroy;
 
 	std::random_device rd; // obtain a random number from hardware
 	std::mt19937 gen{ rd() }; // seed the generator
